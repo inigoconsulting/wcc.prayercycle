68df3f7bb3155476217d629f7fa2e6b99fccc2af
Izhar Firdaus <izhar@inigo-tech.com>
2012-12-13 15:23:50 +0800

    initial package (re #92)

---
+++ setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='wcc.prayercycle',
+      version=version,
+      description="WCC Prayer Cycle",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Inigo Consulting',
+      author_email='team@inigo-tech.com',
+      url='https://github.com/inigoconsulting/wcc.prayercycle',
+      license='gpl',
+      packages=find_packages(),
+      namespace_packages=['wcc'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'plone.app.dexterity',
+          'plone.namedfile [blobs]',
+          # -*- Extra requirements: -*-
+      ],
+      extras_require={
+          'test': [
+              'plone.app.testing',
+           ],
+      },
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      # The next two lines may be deleted after you no longer need
+      # addcontent support from paster and before you distribute
+      # your package.
+      setup_requires=["PasteScript"],
+      paster_plugins=["templer.localcommands"],
+
+      )
