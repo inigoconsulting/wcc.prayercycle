f1787f3ebe9982ff8a5e6a7b79f47893eca4bf45
Izhar Firdaus <izhar@inigo-tech.com>
2013-01-30 09:54:16 +0800

    fix typo

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='wcc.prayercycle',
       version=version,
       description="",
       long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txtt")).read(),
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
