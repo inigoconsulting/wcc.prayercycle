a2e126613230a5d26535713559ad40f2b388b6dd
Izhar Firdaus <izhar@inigo-tech.com>
2013-03-18 12:48:36 +0800

    switch to use wcc.featurable (re #246)

--- wcc/prayercycle/upgrades/handlers.py
+++ wcc/prayercycle/upgrades/handlers.py
@@ -2,3 +2,12 @@ from collective.grok import gs
 from Products.CMFCore.utils import getToolByName
 
 # -*- extra stuff goes here -*- 
+
+
+@gs.upgradestep(title=u'Upgrade wcc.prayercycle to 1002',
+                description=u'Upgrade wcc.prayercycle to 1002',
+                source='1001', destination='1002',
+                sortkey=1, profile='wcc.prayercycle:default')
+def to1002(context):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runAllImportStepsFromProfile('profile-wcc.prayercycle.upgrades:to1002')
--- wcc/prayercycle/upgrades/profiles.py
+++ wcc/prayercycle/upgrades/profiles.py
@@ -1,3 +1,8 @@
 from collective.grok import gs
 
 # -*- extra stuff goes here -*- 
+
+gs.profile(name=u'to1002',
+           title=u'Upgrade wcc.prayercycle to 1002',
+           description='',
+           directory='profiles/to1002')
