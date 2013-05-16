0da142bbbc839bb8354b0ce01aac53eddecc4e6d
Izhar Firdaus <izhar@inigo-tech.com>
2013-05-16 14:40:10 +0800

    added upgrade profile and initial prayer copyright portlet (re #324)

---
+++ wcc/prayercycle/portlet/prayercopyright.py
@@ -0,0 +1,42 @@
+from zope import schema
+from zope.component import getMultiAdapter
+from zope.formlib import form
+from zope.interface import implements
+
+from plone.app.portlets.portlets import base
+from plone.memoize.instance import memoize
+from plone.portlets.interfaces import IPortletDataProvider
+from plone.app.portlets.cache import render_cachekey
+
+from Acquisition import aq_inner
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from wcc.prayercycle import MessageFactory as _
+
+class IPrayerCopyright(IPortletDataProvider):
+    """
+    Define your portlet schema here
+    """
+    pass
+
+class Assignment(base.Assignment):
+    implements(IPrayerCopyright)
+
+    @property
+    def title(self):
+        return _('Prayer Copyright')
+
+class Renderer(base.Renderer):
+    
+    render = ViewPageTemplateFile('templates/prayercopyright.pt')
+
+    @property
+    def available(self):
+        return True
+
+class AddForm(base.NullAddForm):
+    form_fields = form.Fields(IPrayerCopyright)
+    label = _(u"Add Prayer Copyright")
+    description = _(u"")
+
+    def create(self):
+        return Assignment()
